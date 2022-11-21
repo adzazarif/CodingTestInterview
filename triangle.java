0ae2242bbef07a35efdc17e723b214dd8e7543e9
adzazarif <adzazarf@gmail.com>
2022-11-21 15:22:09 +0700

    menambahkan segitiga siku siku kebalik

--- triangle.java
+++ triangle.java
@@ -15,12 +15,26 @@ public class triangle {
         }
     }
     // membuat triangle seperti berikut 
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+    public static void triangle2(){
+        for (int i = banyak; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    // membuat triangle seperti berikut 
     //         *
     //        * *
     //       * * *
     //      * * * *
     //     * * * * *
-    public static void triangle2(){
+    public static void triangle3(){
         for (int i = 1; i <= banyak; i++) {
             for (int j = banyak - 1; j >= i; j--) {
                 System.out.print(" ");
@@ -34,5 +48,6 @@ public class triangle {
     public static void main(String[] args) {
         // triangle1();
         triangle2();
+        // triangle3();
     }
 }
